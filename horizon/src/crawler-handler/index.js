7ab8d8ba1d42f5ac316406ddc1c6b8bffffa9445
Phat Tran <prosandaru@gmail.com>
2020-09-19 15:54:33 +0700

    [Horizon] Defined the Facade Object. Added the crawler-rules. Still working on defining them...

---
+++ horizon/src/crawler-handler/index.js
@@ -0,0 +1,18 @@
+
+const Crawler = {}
+
+/**
+ * Run the crawler
+ * @param {String} url
+ * @param {Object} rules
+ */
+Crawler.make = function (
+    url,
+    rules
+) {
+
+}
+
+export {
+    Crawler
+}
